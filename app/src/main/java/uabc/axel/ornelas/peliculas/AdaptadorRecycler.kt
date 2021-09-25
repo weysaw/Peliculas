a0ca05d24b1021461c04d18f27fbb925efa50b8b
Axel L Ornelas M <axel.lom17@gmail.com>
2021-09-24 22:12:20 -0700

    Se agrego una acción cuando se presiona una pelicula, se agrego la imagen y se documento

--- app/src/main/java/uabc/axel/ornelas/peliculas/AdaptadorRecycler.kt
+++ app/src/main/java/uabc/axel/ornelas/peliculas/AdaptadorRecycler.kt
@@ -3,34 +3,53 @@ package uabc.axel.ornelas.peliculas
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class AdaptadorRecycler(private val localDataSet: ArrayList<Pelicula>) : RecyclerView.Adapter<AdaptadorRecycler.ViewHolder>() {
 
+    //Tal vez se deba poner protected
+    lateinit var onClickListener: View.OnClickListener
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
+        //Indica el estilo que debe tener el recycler
         val view = LayoutInflater.from(parent.context).inflate(R.layout.fila_pelicula, parent, false)
+        view.setOnClickListener(onClickListener)
+        //Devuelve la vista creada
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //Coloca la información de las peliculas a los campos de texto
         holder.nombre.text = localDataSet[position].nombre
         holder.genero.text = localDataSet[position].genero
         holder.rating.text = localDataSet[position].rating.toString()
-
+        holder.imagen.setImageURI(localDataSet[position].imagen)
     }
 
+    /**
+     * Tamaño de los datos
+     */
     override fun getItemCount(): Int {
         return localDataSet.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    /*@JvmName("setOnClickListener1")
+    fun setOnClickListener(onClickListener: View.OnClickListener) {
+        this.onClickListener = onClickListener
+    }*/
+    /**
+     * Clase interna para localizar los datos que se necesitan para colocar la info
+     */
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nombre: TextView = itemView.findViewById(R.id.nombreText)
         val genero: TextView = itemView.findViewById(R.id.generoText)
         val rating: TextView = itemView.findViewById(R.id.ratingText)
-    }
+        val imagen: ImageView = itemView.findViewById(R.id.imagenFila)
 
+    }
 }
